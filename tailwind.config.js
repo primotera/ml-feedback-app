990e7ea92f9506a7b6fbfa376f9e645ee6deb003
primotera <adiop600@gmail.com>
2025-04-04 20:13:48 +0000

    Update Tailwind to support dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'media', // 'media' active le thème sombre quand les préférences système sont au mode sombre
   theme: {
     extend: {},
   },
